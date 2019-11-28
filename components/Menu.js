b5c35ac36d0c5f6c41a5d84fcd246e3d48b0a206
Hector Ibarra <35663477+Hayo412@users.noreply.github.com>
2019-11-28 10:57:55 -0600

    Update-28-11-2019

---
+++ components/Menu.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+
+const Menu = (props) => (
+    <nav id="menu">
+        <div className="inner">
+            <ul className="links">
+                <li><Link href="/"><a><span onClick={props.onToggleMenu}>What we do</span></a></Link></li>
+                <li><Link href="/#"><a><span onClick={props.onToggleMenu}>Work process</span></a></Link></li>
+                <li><Link href="/who"><a><span onClick={props.onToggleMenu}>Who we are</span></a></Link></li>
+                <li><Link href="/#"><a><span onClick={props.onToggleMenu}>Contact Us</span></a></Link></li>
+            </ul>
+            <ul className="actions vertical">
+                <li><a href="#" className="button special fit">Hire Us</a></li>
+            </ul>
+        </div>
+        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
+    </nav>
+)
+
+Menu.propTypes = {
+    onToggleMenu: PropTypes.func
+}
+
+export default Menu
+
+
