1e099ae1cd0d665b6412be61f0356d9f552457b1
Hector Ibarra <35663477+Hayo412@users.noreply.github.com>
2019-12-10 12:06:55 -0600

    Sidebar-logo update

--- components/Menu.js
+++ components/Menu.js
@@ -5,7 +5,7 @@ const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
             <ul className="links">
-                <li><Link href="/"><a><span onClick={props.onToggleMenu}>What we do</span></a></Link></li>
+                <li><Link href="#"><a><span onClick={props.onToggleMenu}>What we do</span></a></Link></li>
                 <li><Link href="/#"><a><span onClick={props.onToggleMenu}>Work process</span></a></Link></li>
                 <li><Link href="/who"><a><span onClick={props.onToggleMenu}>Who we are</span></a></Link></li>
                 <li><Link href="/#"><a><span onClick={props.onToggleMenu}>Contact Us</span></a></Link></li>
